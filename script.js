7f95b9bd366e409f0dd8620c1fc784545a877783
LM&T <tbbragg83@gmail.com>
2020-06-08 16:53:48 -0500

    first commit/psuedo coded

---
+++ script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+    
+//  });
+//use moment.js to incorporate and format date and time
+//current day and time must be appear in the jumbotron
+//hour must start at 9.
+//statements to handle if hour is past, present or future.
+    //might be able to write code to handle each hour in one code???
+//choose which method to use to get items from local storage. getItem() appears the easiest.
+//event listner for save button
