d69f063cc67ceb4de84084e0f030697dac4493b9
LM&T <tbbragg83@gmail.com>
2020-06-09 00:50:32 -0500

    all timeblocks working properly/ working on event handling

--- script.js
+++ script.js
@@ -11,7 +11,14 @@ $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 var roundedTime = moment().startOf("hour");
 var nineAm = moment().startOf('day').add(9, "hours");
-
+var tenAm = nineAm.add(1, "h");
+var elevenAm = tenAm.add(1, "hour");
+var twelvePm = elevenAm.add(1, "hour");
+var onePm = twelvePm.add(1, "hour");
+var twoPm = onePm.add(1, "hour");
+var threePm = twoPm.add(1, "hour");
+var fourPm = threePm.add(1, "hour");
+var fivePm = fourPm.add(1, "hour");
 
 
 
@@ -24,7 +31,88 @@ function myTime() {
     } else
     if (roundedTime.isAfter(nineAm)) {
         $(".time9").addClass("past");
-    }
+    };
+
+    if (roundedTime.isSame(tenAm)) {
+        $(".time10").addClass("present");
+    } else 
+    if (roundedTime.isBefore(tenAm)) {
+        $(".time10").addClass("future");
+    } else
+    if (roundedTime.isAfter(tenAm)) {
+        $(".time10").addClass("past");
+    };
+
+    if (roundedTime.isSame(elevenAm)) {
+        $(".time11").addClass("present");
+    } else 
+    if (roundedTime.isBefore(elevenAm)) {
+        $(".time11").addClass("future");
+    } else
+    if (roundedTime.isAfter(elevenAm)) {
+        $(".time11").addClass("past");
+    };
+
+    if (roundedTime.isSame(twelvePm)) {
+        $(".time12").addClass("present");
+    } else 
+    if (roundedTime.isBefore(twelvePm)) {
+        $(".time12").addClass("future");
+    } else
+    if (roundedTime.isAfter(twelvePm)) {
+        $(".time12").addClass("past");
+    };
+
+    if (roundedTime.isSame(onePm)) {
+        $(".time1").addClass("present");
+    } else 
+    if (roundedTime.isBefore(onePm)) {
+        $(".time1").addClass("future");
+    } else
+    if (roundedTime.isAfter(onePm)) {
+        $(".time1").addClass("past");
+    };
+
+    if (roundedTime.isSame(twoPm)) {
+        $(".time2").addClass("present");
+    } else 
+    if (roundedTime.isBefore(twoPm)) {
+        $(".time2").addClass("future");
+    } else
+    if (roundedTime.isAfter(twoPm)) {
+        $(".time2").addClass("past");
+    };
+
+    if (roundedTime.isSame(threePm)) {
+        $(".time3").addClass("present");
+    } else 
+    if (roundedTime.isBefore(threePm)) {
+        $(".time3").addClass("future");
+    } else
+    if (roundedTime.isAfter(threePm)) {
+        $(".time3").addClass("past");
+    };
+
+    if (roundedTime.isSame(fourPm)) {
+        $(".time4").addClass("present");
+    } else 
+    if (roundedTime.isBefore(fourPm)) {
+        $(".time4").addClass("future");
+    } else
+    if (roundedTime.isAfter(fourPm)) {
+        $(".time4").addClass("past");
+    };
+
+    if (roundedTime.isSame(fivePm)) {
+        $(".time5").addClass("present");
+    } else 
+    if (roundedTime.isBefore(fivePm)) {
+        $(".time5").addClass("future");
+    } else
+    if (roundedTime.isAfter(fivePm)) {
+        $(".time5").addClass("past");
+    };
+    
 
 };
 
